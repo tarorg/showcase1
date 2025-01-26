1c1b3d62e27f4043524ce6c4372fd09e90d9f8df
tarorg <tarframework@gmail.com>
2025-01-26 09:31:46 +0530

    m4

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
-import autoAdapter from '@sveltejs/adapter-auto';
-import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: cloudflareAdapter(), // Use the desired adapter here
-    // ... other configuration ...
-  }
+export default {
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	}
 };
-
-export default config;
